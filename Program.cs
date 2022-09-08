14cc0428576fea4f513162d40f065272fdb3b529
AndreyAnisimovTerricon <atriummebel79@yandex.ru>
2022-09-08 22:07:30 +0300

    Координаты точки пересечения прямых

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Введите b1 :");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите k1 :");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите b2 :");
+double b2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите k2 :");
+ double k2 = double.Parse(Console.ReadLine());
+double x = (b2-b1)/(k1-k2);
+double y = k1*x+b1;
+Console.Write("Точка пересечения прямых имеет координаты X: ");
+Console.Write(x);
+Console.Write("  ; Y: ");
+Console.Write(y);
